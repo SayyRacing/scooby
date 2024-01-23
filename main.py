ee9f452734b9f331f9dc90f0b2539268ef4612b4
Sayy <szymonpaluszewski@gmail.com>
2024-01-23 18:11:16 +0100

    feat: Added searching through the first page

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World! 2137")
+import requests
+from bs4 import BeautifulSoup
+
+try:
+    source = requests.get('https://poszukiwani.policja.pl/pos/form/5,Poszukiwani.html')
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text, 'html.parser')
+
+    wanted = soup.find_all('li', class_='threeRows thumbList')
+
+    for person in wanted:
+
+        wanted = person.find('strong').text
+        print(wanted)
+
+except Exception as e:
+    print(e)
